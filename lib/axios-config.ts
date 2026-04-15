39653b0e11bb26a7efc9b378b18cc57f5b7c89cc
Kendall <kendallsalazarsoto@gmail.com>
2026-04-15 14:35:53 -0600

    Using .env for baseURL

--- lib/axios-config.ts
+++ lib/axios-config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://localhost:7121/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
